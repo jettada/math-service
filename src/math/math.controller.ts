8d6b78f0bd8cc135fc0ab93b8d7021a0a107a2ce
Jettada Kongchai <64160044@go.buu.ac.th>
2024-09-08 20:38:44 +0700

    Call Sum Message

--- src/math/math.controller.ts
+++ src/math/math.controller.ts
@@ -1,13 +1,21 @@
 import { Body, Controller, Logger, Post } from '@nestjs/common';
 import { MathService } from './math.service';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller('math')
 export class MathController {
   private logger = new Logger('MathController');
   constructor(private readonly mathService: MathService) {}
-  @Post('sum')
+
+  @Post('sum1')
   sum(@Body('numbers') numbers: number[]): number {
     this.logger.log('sum ' + numbers);
+    return this.mathService.accumulate(numbers);
+  }
+
+  @MessagePattern({ cmd: 'sum' })
+  sum2(numbers: number[]): number {
+    this.logger.log('sum2 ' + numbers);
     return this.mathService.accumulate(numbers);
   }
 }
